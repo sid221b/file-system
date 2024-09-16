af5eb5becb72bda8005111e2348dc143e93d6863
Sudhanshu <sudhanshu221b@gmail.com>
2024-09-17 00:58:47 +0530

    added new files sys

---
+++ src/components/File.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import initialData from '../utils/initialData'
+import FileList from './FileList';
+
+function File() {
+  const [files, setFiles] = useState(initialData)
+  const [inputVal, setInputVal] = useState('')
+
+  const onExpandClick = (path) => {
+    const newFilesList = [...files]
+    let nextFiles = newFilesList
+    path?.forEach((curPath) => {
+      if(Array.isArray(nextFiles)) {
+        nextFiles = nextFiles[nextFiles.findIndex(itm => itm.name === curPath)]
+      } else if(nextFiles?.files?.length > 0) {
+        nextFiles = nextFiles?.files?.[nextFiles?.files?.findIndex?.(itm => itm.name === curPath)]
+      }
+    })
+    nextFiles.isOpen = !nextFiles.isOpen
+    setFiles(newFilesList)
+  }
+
+  const onFileDblClick = (path) => {
+    const newFilesList = [...files]
+    let nextFiles = newFilesList
+    path?.forEach((curPath) => {
+      if(Array.isArray(nextFiles)) {
+        nextFiles = nextFiles[nextFiles.findIndex(itm => itm.name === curPath)]
+      } else if(nextFiles?.files?.length > 0) {
+        nextFiles = nextFiles?.files?.[nextFiles?.files?.findIndex?.(itm => itm.name === curPath)]
+      }
+    })
+    nextFiles.isOpen = !nextFiles.isOpen
+    nextFiles.files = []
+    setFiles(newFilesList)
+  }
+
+  const onAddFile = (path) => {
+    if(!inputVal) {
+      alert('Please enter a file name in the input box')
+      return
+    }
+    const newFilesList = [...files]
+    let nextFiles = newFilesList
+    path?.forEach((curPath) => {
+      if(Array.isArray(nextFiles)) {
+        nextFiles = nextFiles[nextFiles.findIndex(itm => itm.name === curPath)]
+      } else if(nextFiles?.files?.length > 0) {
+        nextFiles = nextFiles?.files?.[nextFiles?.files?.findIndex?.(itm => itm.name === curPath)]
+      }
+    })
+    if(Array.isArray(nextFiles)) {
+      nextFiles.push({name: inputVal})
+    } else {
+      nextFiles.files = [...nextFiles.files, {name: inputVal}]
+    }
+    setFiles(newFilesList)
+    setInputVal('')
+  }
+
+  return (
+    <div className='layout-row justify-content-between'>
+        <ul data-testid="files">
+           <FileList files={files} onExpand={onExpandClick} makeDir={onFileDblClick} onAddFile={onAddFile} />
+        </ul>
+        <input data-testid="input-box" className='mt-15 mr-35 w-15' style={{borderColor: "black"}} type="text" placeholder='Enter an item' value={inputVal} onChange={e => setInputVal(e.target.value)} />
+    </div>
+  )
+}
+
+export default File
---
+++ src/components/FileList.js
@@ -0,0 +1,41 @@
+import React, { memo } from 'react';
+
+function FileData({onExpand, name, isExpandable, isOpen, makeDir, files}) {
+    return (
+      <>
+        <li>
+        <button
+          onClick={onExpand}
+          onDoubleClick={makeDir}
+        >
+          {name}
+          {isExpandable && <span>{isOpen ? '[-]' : '[+]'}</span>}
+        </button>
+      </li>
+      </>
+    );
+  }
+
+function FileList({files, onExpand, path, makeDir, onAddFile}) {
+    return (
+      <ul>{
+        files.map((data, index) => {
+          const isExpandable = Array.isArray(data?.files)
+          const newPath = path ? [...path, data.name] : [data.name]
+        return(
+        <>
+        <FileData name={data.name} onExpand={() => onExpand(newPath)} isExpandable={isExpandable} isOpen={data?.isOpen} makeDir={() => makeDir(newPath)} />
+        {data?.isOpen && isExpandable && (
+          <FileList files={data.files} onExpand={onExpand} path={newPath} onAddFile={onAddFile} makeDir={makeDir}  />
+        )}
+    </>
+    )})
+      }
+      <li>
+        <button onClick={() => onAddFile(path || [])}>+</button>
+    </li>
+      </ul>
+  )
+}
+
+export default memo(FileList);
